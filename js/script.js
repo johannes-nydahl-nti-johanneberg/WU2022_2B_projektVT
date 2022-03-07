c01e41cf04ed31460e39b267c087b8b799c32e56
johannes-nydahl-nti-johanneberg <mobiltidag@gmail.com>
2022-03-07 21:11:57 +0100

    Gjorde klart animationen för navigationsmeny-knappen

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector("#burger");
+const navMenu = document.querySelector("#nav_menu");
+const body = document.querySelector("body");
+
+burger.addEventListener("click", () => {
+	navMenu.classList.toggle("nav-active");
+	burger.classList.toggle("toggle");
+	body.classList.toggle("toggle");
+});
