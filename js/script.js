d21241c083f181a54ec376be6cff64e1495a711a
johannes-nydahl-nti-johanneberg <mobiltidag@gmail.com>
2022-03-22 16:12:16 +0100

    Fixat nav-system och börjat lägga till sections

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 const burger = document.querySelector("#burger");
-const navMenu = document.querySelector("#nav_menu");
+const navMenu = document.querySelector("#navmenu");
+const navBar = document.querySelector(".navbar");
 const body = document.querySelector("body");
 
+let lastScrollY = window.scrollY;
+
 burger.addEventListener("click", () => {
-	navMenu.classList.toggle("nav-active");
+	navMenu.classList.toggle("nav_active");
 	burger.classList.toggle("toggle");
-	body.classList.toggle("no-scroll");
+	body.classList.toggle("no_scroll");
+	if (navBar.classList.contains("navbar_top") && navMenu.classList.contains("nav_active")) {
+		navBar.classList.remove("navbar_top");
+	} else if (!navMenu.classList.contains("nav_active") && window.scrollY === 0) {
+		navBar.classList.add("navbar_top");
+	}
+});
+
+window.addEventListener("scroll", () => {
+	if (lastScrollY < window.scrollY) {
+		navBar.classList.add("navbar_hidden");
+	} else {
+		navBar.classList.remove("navbar_hidden");
+		if (window.scrollY > 0) {
+			navBar.classList.remove("navbar_top");
+		} else {
+			navBar.classList.add("navbar_top");
+		}
+	}
+	lastScrollY = window.scrollY;
 });
